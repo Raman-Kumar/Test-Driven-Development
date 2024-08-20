086d9388e9794308f6b89aa906e189746609db0b
Raman kumar <k.raman1998@yahoo.in>
2024-08-21 00:17:46 +0530

    adding tests for add function

---
+++ TDD-String-Calc/src/utils/add.test.js
@@ -0,0 +1,19 @@
+import { add } from './add'
+
+describe('testing add function', ()=> {
+
+    test(' add("") should be 0 ', ()=> {
+        expect(add("1,2")).toBe(0)
+
+    })
+
+    test(' add("1") should be 1 ', ()=> {
+        expect(add("1")).toBe(1)
+    })
+
+    test(' add("1,2") should be 3 ', ()=> {
+        expect(add("1,2")).toBe(3)
+    })
+
+
+})
