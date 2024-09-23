ae51c355b4705458797bd3f5076e41fd7ca58f49
Raman kumar <k.raman1998@yahoo.in>
2024-09-23 18:14:31 +0530

    fix spelling

--- TDD-String-Calc/src/utils/add.js
+++ TDD-String-Calc/src/utils/add.js
@@ -2,23 +2,23 @@ export const add = (string) => {
     if(!string) return 0
 
     let delimiter = '\n'
-    let stringArry = string
+    let stringArray = string
     if(string.substring(0,2) == '//'){
-        delimiter = stringArry.substring(2,3)
-        stringArry = string.substring(4)
-        if(stringArry.charAt(0) == 'n')
-            stringArry = string.substring(5)
+        delimiter = stringArray.substring(2,3)
+        stringArray = string.substring(4)
+        if(stringArray.charAt(0) == 'n')
+            stringArray = string.substring(5)
     }
-    stringArry = stringArry.split(delimiter).map(item => item.split(','))
-    let numbersArray = stringArry.flat()
+    stringArray = stringArray.split(delimiter).map(item => item.split(','))
+    let numbersArray = stringArray.flat()
 
-    let nagativeNums = []
+    let negativeNums = []
     numbersArray.map(num => {
         if(num < 0)
-        nagativeNums.push(num)
+            negativeNums.push(num)
     });
-    if(nagativeNums.length)
-        throw new Error(`negative numbers not allowed: ${nagativeNums.join(',')}`)
+    if(negativeNums.length)
+        throw new Error(`negative numbers not allowed: ${negativeNums.join(',')}`)
 
     const result = numbersArray.reduce((acc, num) => {
         acc = acc + parseInt(num)
